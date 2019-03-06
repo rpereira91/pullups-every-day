bb18ca7bf8d6beea4c01d5924ab405b1fb82cbcb
Unknown <Rp09rl@brocku.ca>
2019-03-06 15:09:36 -0500

    media query

--- src/components/DisplaySets.js
+++ src/components/DisplaySets.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import './style/App.css';
+import MediaQuery from 'react-responsive';
 
 export default class DisplaySets extends Component {
   render() {
     return (
       <div>
+        <MediaQuery minDeviceWidth={1224}>
         {this.props.set.map((reps, index) => 
               <div className='set'>
               Set: {index+1} : {reps}
               </div>
             )}
+        </MediaQuery>
+        <MediaQuery minDeviceWidth={480}>
+          <div>You are portrait</div>
+      </MediaQuery>
       </div>
     )
   }
